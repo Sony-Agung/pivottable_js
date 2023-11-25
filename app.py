d8071f9a47a22ea95f01c3f241cb5e4031287bf8
Sony Agung <124691368+Sony-Agung@users.noreply.github.com>
2023-11-26 02:35:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,17 @@ import streamlit as st
 
 # Membaca file HTML dengan pivot table JavaScript
 with open("files/pivottablejs.html", "r") as file:
-    html_content = file.read()
+    pivottable_html_content = file.read()
 
-# Menyematkan konten HTML di dalam aplikasi Streamlit
-st.components.v1.html(html_content, height=800, width=1200, scrolling=True)
+with open("files/folium_map.html", "r") as file:
+    folium_html_content = file.read()
+
+# Display title for PivotTable
+st.markdown("# PivotTable")
+# Display PivotTable
+st.components.v1.html(pivottable_html_content, height=400, width=2000, scrolling=True)
+
+# Display title for Folium map
+st.markdown("# Folium Map")
+# Display Folium map
+st.components.v1.html(folium_html_content, height=500, width=800, scrolling=True)
