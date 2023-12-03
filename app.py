bc7c089df9abc398af53f5d2475dc60242d9026c
Sony Agung <124691368+Sony-Agung@users.noreply.github.com>
2023-12-03 12:39:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Membaca file HTML dengan pivot table JavaScript
-with open("files/pivottablejs.html", "r") as file:
+with open("files/pivottablejs (1).html", "r") as file:
     pivottable_html_content = file.read()
  
 with open("files/folium_maps.html", "r") as file:
